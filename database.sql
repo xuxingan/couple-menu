39bf8ce3c9345ed99f1d6e0cd688ffbf15b48f65
xuxingan <xuxingan@qq.com>
2024-12-11 16:15:48 +0800

    readme

--- database.sql
+++ database.sql
@@ -7,12 +7,4 @@ create table dishes (
   created_by text not null, -- 'male' 或 'female'
   created_at timestamp with time zone default timezone('utc'::text, now()),
   wished boolean default false
-);
-
--- 期待清单表
-create table wish_list (
-  id uuid default uuid_generate_v4() primary key,
-  dish_id uuid references dishes(id),
-  wished_by text not null, -- 'male' 或 'female'
-  created_at timestamp with time zone default timezone('utc'::text, now())
 ); 
